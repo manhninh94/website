2656e2e41c49156bb09efe86e3e3e53e61ef43f7
manhninh <tmninh94@gmail.com>
2016-12-04 13:08:35 +0700

    up

--- component/connect.php
+++ component/connect.php
@@ -1,8 +1,9 @@
-<?php
-// kết nối với database
-   $connect =mysql_connect("localhost","root","") or die ("Lỗi kết nối cơ sở dữ liệu");
-  
- // Ket noi với data ss
-   mysql_select_db("data ss",$connect) or die ("Lỗi kết nối cơ sở dữ liệu");
-   
+<?php 
+    $con=mysql_connect("mysql.hostinger.vn","u427760457_root","ninh1994");
+    if(!$con)
+    {
+    die('could not connect'. mysql_error());
+    }
+    mysql_select_db("u427760457_simso",$con);
+    mysql_set_charset('utf-8');
 ?>
--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php 
-    $con=mysql_connect("localhost","root","");
+    $con=mysql_connect("mysql.hostinger.vn","u427760457_root","ninh1994");
     if(!$con)
     {
     die('could not connect'. mysql_error());
     }
-    mysql_select_db("db_websimso",$con);
-    mysql_set_charset('utf8',$con);
+    mysql_select_db("u427760457_simso",$con);
+    mysql_set_charset('utf-8');
 ?>
